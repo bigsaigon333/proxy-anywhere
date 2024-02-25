6969e39b9d5e60cb17a108849f26d0815881f76b
bigsaigon333 <likepeppermint@gmail.com>
2024-02-25 20:54:51 +0900

    feat: 하나의 referer에 대해서 여러개의 proxyMap을 설정할 수 있다

--- src/services/proxyRule.ts
+++ src/services/proxyRule.ts
@@ -7,39 +7,33 @@ export type ProxyRule = {
   queryString?: Record<string, string | number>;
 };
 
-export const proxyRuleMap = new Map<string, ProxyRule>([
+export const proxyRuleMap = new Map<string, ProxyRule[]>([
   [
     "https://bigsaigon333.me",
-    {
-      path: "/naver",
-      target: "https://sports.news.naver.com",
-      rewritePath: {
-        "^/naver": "",
+    [
+      {
+        path: "/naver",
+        target: "https://sports.news.naver.com",
+        rewritePath: {
+          "^/naver": "",
+        },
+        queryString: {
+          abcd: "efg",
+          "1234": 5678,
+        },
       },
-      queryString: {
-        abcd: "efg",
-        "1234": 5678,
-      },
-    },
+    ],
   ],
 ]);
 
 export function getProxyRule(key: string, path: string): ProxyRule | null {
-  const proxyRule = proxyRuleMap.get(key);
+  const proxyRuleList = proxyRuleMap.get(key) ?? [];
+
+  const proxyRule = proxyRuleList.find((rule) =>
+    micromatch.isMatch(path, rule.path),
+  );
 
   if (proxyRule == null) return null;
 
-  if (!micromatch.isMatch(path, proxyRule.path)) return null;
-
   return proxyRule;
 }
-
-/*
-{
-  [referer]: {
-    target: string;
-    rewritePath?: Record<string, string>;
-    queryString?: Record<string, string>;
-  }[]
-}
-*/
